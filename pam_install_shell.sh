5c52469bb3b11e421c0a3f175982a16a88f4e8ec
zhengjim <827301757@qq.com>
2022-02-10 11:16:21 +0800

    2022-02-10 add pam-python-1.0.7

--- pam_install_shell.sh
+++ pam_install_shell.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-echo "start install ..."
+echo "Start install ..."
 yum -y install gcc make pam pam-devel python-devel
-wget -O pam-python-1.0.7.tar.gz https://sourceforge.net/projects/pam-python/files/pam-python-1.0.7-1/pam-python-1.0.7.tar.gz/download --no-check-certificate
 tar zxf pam-python-1.0.7.tar.gz
 cd pam-python-1.0.7/src
-sed -i '43d' pam_python.c
-sed -i  "30a #include <Python.h>" pam_python.c
 make pam_python.so
 if [ ! -f pam_python.so ]; then
   echo "Error: compile pam_python.so fail!"
@@ -20,4 +17,4 @@ sed -i 's#^ChallengeResponseAuthentication no#ChallengeResponseAuthentication ye
 echo 'auth requisite pam_python.so /usr/lib64/security/pam_notice_auth.py' >> /etc/pam.d/sshd
 systemctl restart sshd
 
-echo "ok"
+echo "installation is complete..."
--- pam_uninstall_shell.sh
+++ pam_uninstall_shell.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-echo "start uninstall ..."
+echo "Start uninstall ..."
 
 sed -i 's#^ChallengeResponseAuthentication yes#ChallengeResponseAuthentication no#' /etc/ssh/sshd_config
 sed -i '/pam_notice_auth/d' /etc/pam.d/sshd
 rm -rf /usr/lib64/security/pam_python.so
 rm -rf /usr/lib64/security/pam_auth.py
 rm -rf pam-python-1.0.7
-rm -rf pam-python-1.0.7.tar.gz
 systemctl restart sshd
 
-echo "ok..."
+echo "Uninstall complete ..."
